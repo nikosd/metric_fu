a396ccefffeb760769c1eda154c6811717b813f5
Kevin Scaldeferri <kevin@scaldeferri.com>
2009-09-22 23:31:16 -0700

    make graphs start at 0

--- lib/graphs/reek_grapher.rb
+++ lib/graphs/reek_grapher.rb
@@ -1,19 +1,19 @@
 module MetricFu
-  
+
   class ReekGrapher < Grapher
-    
+
     attr_accessor :reek_count, :labels
-    
+
     def initialize
       super
       self.reek_count = {}
       self.labels= {}
     end
-    
+
     def get_metrics(metrics, date)
       counter = self.labels.size
       self.labels.update( { counter => date })
-      
+
       metrics[:reek][:matches].each do |reek_chunk|
         reek_chunk[:code_smells].each do |code_smell|
           # speaking of code smell...
@@ -22,7 +22,7 @@ module MetricFu
         end
       end
     end
-    
+
     def graph!
       g = Gruff::Line.new(MetricFu.graph_size)
       g.title = "Reek: code smells"
@@ -32,13 +32,14 @@ module MetricFu
         g.data(type, count)
       end
       g.labels = self.labels
+      g.minimum_value = 0
       g.title_font_size = MetricFu.graph_title_font_size
       g.legend_box_size = MetricFu.graph_legend_box_size
       g.legend_font_size = MetricFu.graph_legend_font_size
       g.marker_font_size = MetricFu.graph_marker_font_size
       g.write(File.join(MetricFu.output_directory, 'reek.png'))
     end
-    
+
   end
-  
+
 end
