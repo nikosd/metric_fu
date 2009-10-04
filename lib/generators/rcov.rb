nikosd/metric_fu

commit 03335b9b45c56fbdc4c3b04ddd4d6e8942e8923a

Navigation bar in the header.

lib/templates/awesome/css/navigation.css
@@ -1,0 +1,23 @@
+#header h1 {
+  font-size: 1.5em;
+  font-weight: bold;
+  left: 1.3em;
+  position: absolute;
+}
+#header #projects {
+    display: block;
+    font-size: 1.4em;
+    font-weight: bold;
+    height: 1.3em;
+    line-height: 1.3em;
+    text-align: center;
+  }
+  #header #projects li{
+    display: inline-block;
+    margin-left: 1em;
+  }
+  #header #projects li.active{
+    background-color:#ED1556;
+    color:#FFFFFF;
+    }
+    #header #projects li.active a{ color:#FFFFFF; }

lib/templates/awesome/index.html.erb
@@ -1,28 +1,2 @@
 <h3>Metric Fu Results</h3>
-<ul id='projects'>
-<% if @churn %>
-  <li class='even failure'><a href="churn.html">Churn</a></li>
-<% end %>
-<% if @flay %>
-  <li class='even failure'><a href="flay.html">Flay</a></li>
-<% end %>
-<% if @flog %>
-  <li class='even failure'><a href="flog.html">Flog</a></li>
-<% end %>
-<% if @rcov %>
-  <li class='even failure'><a href="rcov.html">Rcov</a></li>
-<% end %>
-<% if @reek %>
-  <li class='even failure'><a href="reek.html">Reek</a></li>
-<% end %>
-<% if @roodi %>
-  <li class='even failure'><a href="roodi.html">Roodi</a></li>
-<% end %>
-<% if @saikuro %>
-  <li class='even failure'><a href="saikuro.html">Saikuro</a></li>
-<% end %>
-<% if @stats %>
-  <li class='even failure'><a href="stats.html">Stats</a></li>
-<% end %>
-</ul>
 <p>Generated on <%= Time.now.localtime %></p>	

lib/templates/awesome/layout.html.erb
@@ -8,6 +8,7 @@
     <%= inline_css("css/reset.css") %>
     <%= inline_css("css/buttons.css") %>
     <%= inline_css("css/integrity.css") %>
+    <%= inline_css("css/navigation.css") %>
     <%= inline_css("css/default.css") %>
     </style>
     <link REL="SHORTCUT ICON" HREF="/favicon.ico">
@@ -15,6 +16,34 @@
   <body>
     <div id='header'>
       <h1><a href="/">metrics</a> | <a href="index.html"><%= @name %></a></h1>
+
+      <ul id='projects'>
+      <% if @churn %>
+         <li class='even failure <%= (@current_section == :churn) ? "active" : "" %>'><a href="churn.html">Churn</a></li>
+      <% end %>
+      <% if @flay %>
+        <li class='even failure <%= (@current_section == :flay) ? "active" : "" %>'><a href="flay.html">Flay</a></li>
+      <% end %>
+      <% if @flog %>
+        <li class='even failure <%= (@current_section == :flog) ? "active" : "" %>'><a href="flog.html">Flog</a></li>
+      <% end %>
+      <% if @rcov %>
+        <li class='even failure <%= (@current_section == :rcov) ? "active" : "" %>'><a href="rcov.html">Rcov</a></li>
+      <% end %>
+      <% if @reek %>
+        <li class='even failure <%= (@current_section == :reek) ? "active" : "" %>'><a href="reek.html">Reek</a></li>
+      <% end %>
+      <% if @roodi %>
+        <li class='even failure <%= (@current_section == :roodi) ? "active" : "" %>'><a href="roodi.html">Roodi</a></li>
+      <% end %>
+      <% if @saikuro %>
+        <li class='even failure <%= (@current_section == :saikuro) ? "active" : "" %>'><a href="saikuro.html">Saikuro</a></li>
+      <% end %>
+      <% if @stats %>
+        <li class='even failure <%= (@current_section == :stats) ? "active" : "" %>'><a href="stats.html">Stats</a></li>
+      <% end %>
+      </ul>
+
       <address class='watermark'>
         built with
         <a href='http://metric-fu.rubyforge.org/'>metric-fu</a>

lib/templates/awesome/awesome_template.rb
@@ -4,10 +4,19 @@ class AwesomeTemplate < MetricFu::Template
     # Getting rid of the crap before and after the project name from integrity
     @name = File.basename(Dir.pwd).gsub(/^\w+-|-\w+$/, "")
 
+    # Create the instance variables first so we can have them available for
+    # each report page (they are used for the navigation links)
     report.each_pair do |section, contents|
       if template_exists?(section)
         create_instance_var(section, contents)
+      end
+    end
+
+    # Generate the actual report pages
+    report.each_pair do |section, contents|
+      if template_exists?(section)
         @html = erbify(section)
+        @current_section = section
         html = erbify('layout')
         fn = output_filename(section)
         MetricFu.report.save_output(html, MetricFu.output_directory, fn)
@@ -17,6 +26,7 @@ class AwesomeTemplate < MetricFu::Template
     # Instance variables we need should already be created from above
     if template_exists?('index')
       @html = erbify('index')
+      @current_section = :index
       html = erbify('layout')
       fn = output_filename('index')
       MetricFu.report.save_output(html, MetricFu.output_directory, fn)

commit b507933d2761c5b7acd93ea3deed39ed7aa2cc76

Totals are now shown on the index page (merged and resolved conflicts caused by 03335b9b45)

lib/generators/rcov.rb
@@ -86,7 +86,11 @@ module MetricFu
 
     def to_h
       global_percent_run = ((@global_total_lines_run.to_f / @global_total_lines.to_f) * 100)
-      {:rcov => @rcov.merge({:global_percent_run => round_to_tenths(global_percent_run) })}
+      result = @rcov.merge({:global_percent_run => round_to_tenths(global_percent_run) })
+      def result.files
+        select {|k,v| k != :global_percent_run}
+      end
+      {:rcov => result}
     end
 
     private

lib/templates/awesome/css/integrity.css
@@ -210,6 +210,9 @@ a {
           color: #337022; }
         #content #projects li a.failed {
           color: #ff1100; }
+        #content #projects li a span.totals {
+          font-size: 0.5em;
+          padding-left: 1em;}
       #content #projects li .meta {
         position: absolute;
         right: .6em;

lib/templates/awesome/css/navigation.css
@@ -15,7 +15,11 @@
   #header #projects li{
     display: inline-block;
     margin-left: 1em;
-  }
+    }
+    #header #projects li a span.totals {
+      font-size: 0.5em;
+      padding-left: 1em;
+    }
   #header #projects li.active{
     background-color:#ED1556;
     color:#FFFFFF;

lib/templates/awesome/index.html.erb
@@ -1,2 +1,2 @@
 <h3>Metric Fu Results</h3>
-<p>Generated on <%= Time.now.localtime %></p>	
+<p>Generated on <%= Time.now.localtime %></p>

lib/templates/awesome/layout.html.erb
@@ -19,9 +19,22 @@
 
       <ul id='projects'>
       <% %w{churn flay flog rcov reek roodi saikuro stats}.each do |section| %>
-        <% if eval("@#{section}") %>
+        <% if eval("@#{section}")
+            totals = case section.to_sym
+              when :flay then @flay[:total_score]
+              when :flog then @flog[:total] / @flog[:average]
+              when :rcov then @rcov[:global_percent_run]
+              when :stats then "#{@stats[:codeLOC]} lines"
+              else nil
+            end
+        %>
           <li class='even failure <%= (@current_section == section.to_sym) ? "active" : "" %>'>
-            <a href="<%= "#{section}.html" %>"><%= section.capitalize %></a>
+            <a href="<%= "#{section}.html" %>">
+              <%= section.capitalize %>
+              <% if totals %>
+                <span class='totals'>(<%= totals %>)</span>
+              <% end %>
+            </a>
           </li>
         <% end %>
       <% end %>

lib/templates/awesome/rcov.html.erb
@@ -4,14 +4,14 @@
 
 <img src="rcov.png?<%= Time.now %>">
 
-<p>Total Coverage: <%= @rcov.delete(:global_percent_run) %>% </p>
+<p>Total Coverage: <%= @rcov[:global_percent_run] %>% </p>
 <table>
   <tr>
     <th>File Path</th>
     <th>Percent run</th>
   </tr>
   <% count = 0 %>
-  <% @rcov.sort_by{ |k,v| v[:percent_run] }.each do |fname, file| %>
+  <% @rcov.files.sort_by{ |k,v| v[:percent_run] }.each do |fname, file| %>
     <tr>
       <td><a href="#<%= fname.gsub(/[^a-z]+/, '_') %>"><%= fname %></a></td>
       <td><%= file[:percent_run] %></td>
@@ -20,7 +20,7 @@
   <% end %>
 </table>
 
-<% @rcov.sort_by{ |k,v| v[:percent_run] }.each do |fname, file| %>
+<% @rcov.files.sort_by{ |k,v| v[:percent_run] }.each do |fname, file| %>
   <h2 id="<%= fname.gsub(/[^a-z]+/, '_') %>"> <%= fname %></h2>
   <div class="rcov_overflow">
     <table class="rcov_code">