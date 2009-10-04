7a6d60ec50c5b225a298ce92eac5c03f233a64e7
Nikos Dimitrakopoulos <nospam@nikosd.com>
2009-10-04 16:47:18 +0300

    Spec stub for commit 03335b9b45.

--- spec/templates/awesome_template_spec.rb
+++ spec/templates/awesome_template_spec.rb
@@ -3,5 +3,6 @@ require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 describe AwesomeTemplate do
   describe "#write" do
     it "should set the name correctly for builds on CC.rb" # Untested feature on commit 4fa0853b1e
+    it "should set the @current_section correctly for each section" # Untested feature on commit 03335b9b45
   end
 end
