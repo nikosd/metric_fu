2a8876331b73cfba6d26ee90cc503fb5bdaa4d31
Nikos Dimitrakopoulos <nospam@nikosd.com>
2009-10-04 16:45:29 +0300

    Spec stub for previous commit.

---
+++ spec/templates/awesome_template_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
+
+describe AwesomeTemplate do
+  describe "#write" do
+    it "should set the name correctly for builds on CC.rb" # Untested feature on commit 4fa0853b1e
+  end
+end
