03335b9b45c56fbdc4c3b04ddd4d6e8942e8923a
Nikos Dimitrakopoulos <nospam@nikosd.com>
2009-10-04 14:50:48 +0300

    Navigation bar in the header.

--- lib/templates/awesome/awesome_template.rb
+++ lib/templates/awesome/awesome_template.rb
@@ -4,10 +4,19 @@ class AwesomeTemplate < MetricFu::Template
     # Getting rid of the crap before and after the project name from integrity
     @name = File.basename(Dir.pwd).gsub(/^\w+-|-\w+$/, "")
 
+    # Create the instance variables first so we can have them available for
+    # each report page (they are used for the navigation links)
     report.each_pair do |section, contents|
       if template_exists?(section)
         create_instance_var(section, contents)
+      end
+    end
+
+    # Generate the actual report pages
+    report.each_pair do |section, contents|
+      if template_exists?(section)
         @html = erbify(section)
+        @current_section = section
         html = erbify('layout')
         fn = output_filename(section)
         MetricFu.report.save_output(html, MetricFu.output_directory, fn)
@@ -17,6 +26,7 @@ class AwesomeTemplate < MetricFu::Template
     # Instance variables we need should already be created from above
     if template_exists?('index')
       @html = erbify('index')
+      @current_section = :index
       html = erbify('layout')
       fn = output_filename('index')
       MetricFu.report.save_output(html, MetricFu.output_directory, fn)
