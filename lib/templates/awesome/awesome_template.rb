0d4338db2c0120ae30ccd2f7ec2040e73039f5aa
Nikos Dimitrakopoulos <nospam@nikosd.com>
2009-10-09 23:44:24 +0300

    And the original awesome has no navigatior bar once again.

--- lib/templates/awesome/awesome_template.rb
+++ lib/templates/awesome/awesome_template.rb
@@ -5,19 +5,11 @@ class AwesomeTemplate < MetricFu::Template
     dir = MetricFu.configuration.is_cruise_control_rb? ? File.join(Dir.pwd, '..') : Dir.pwd
     @name =  File.basename(File.expand_path(dir)).gsub(/^\w+-|-\w+$/, "")
 
-    # Create the instance variables first so we can have them available for
-    # each report page (they are used for the navigation links)
-    report.each_pair do |section, contents|
-      if template_exists?(section)
-        create_instance_var(section, contents)
-      end
-    end
-
     # Generate the actual report pages
     report.each_pair do |section, contents|
       if template_exists?(section)
+        create_instance_var(section, contents)
         @html = erbify(section)
-        @current_section = section
         html = erbify('layout')
         fn = output_filename(section)
         MetricFu.report.save_output(html, MetricFu.output_directory, fn)
@@ -27,7 +19,6 @@ class AwesomeTemplate < MetricFu::Template
     # Instance variables we need should already be created from above
     if template_exists?('index')
       @html = erbify('index')
-      @current_section = :index
       html = erbify('layout')
       fn = output_filename('index')
       MetricFu.report.save_output(html, MetricFu.output_directory, fn)
